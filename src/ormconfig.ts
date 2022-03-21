1b876a4f25792039a8c31495d647dc50343e6deb
rachkovartem <ateee@yandex.ru>
2022-03-21 18:59:04 +0300

    fix: config type orm

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,6 +1,17 @@
 import { ConnectionOptions } from 'typeorm';
 console.log(process.env.npm_lifecycle_event)
-const config: ConnectionOptions = process.env.npm_lifecycle_event === 'start:dev' ? {
+console.log(process.env.DATABASE_URL)
+
+const config: ConnectionOptions = process.env.DATABASE_URL ? {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+} : {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
@@ -13,19 +24,7 @@ const config: ConnectionOptions = process.env.npm_lifecycle_event === 'start:dev
   cli: {
     migrationsDir: 'src/migrations',
   },
-} : {
-  type: 'postgres',
-  host: 'ec2-54-76-249-45.eu-west-1.compute.amazonaws.com',
-  port: 5432,
-  username: 'kxsbfojdzhmzpx',
-  password: '5dfcdfcfa54be27f6b5da031ea35cbd304229ff19d3dc24ae46bca205f47a6fc',
-  database: 'dep5ke4na717s0',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
-  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-  cli: {
-    migrationsDir: 'src/migrations',
-  },
+
 };
 
 export default config;
