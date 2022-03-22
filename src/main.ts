d74913965efcda6b9fe92ca437c8840c88ad35fa
rachkovartem <ateee@yandex.ru>
2022-03-22 11:39:39 +0300

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://nextchat-app.herokuapp.com', '193.176.84.208'],
+  // origin: ['http://localhost:3000', 'https://nextchat-app.herokuapp.com', '193.176.84.208'],
   credentials: true,
-  optionSuccessStatus: 200,
+  // optionSuccessStatus: 200,
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
   app.enableCors(corsOptions);
   app.use(cookieParser());
   await app.listen(process.env.PORT || 8080);
