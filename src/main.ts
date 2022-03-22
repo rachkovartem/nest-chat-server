6c45e6386a1fd97fff43835a6afcc7ce9b853283
rachkovartem <ateee@yandex.ru>
2022-03-22 10:46:25 +0300

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
   app.use(cookieParser());
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
