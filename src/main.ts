c88d8173c3d0839096569de7ce6035ae9bb4a54e
rachkovartem <ateee@yandex.ru>
2022-03-22 15:27:58 +0300

    fix: vercel

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-const cors = require('cors')
 
 const corsOptions = {
   origin: ['https://nextchat-app.herokuapp.com', 'http://localhost:3000', '193.176.84.208'],
   credentials: true,
   optionSuccessStatus: 200,
-  // origin: true,
-  // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  // preflightContinue: false,
-  // optionsSuccessStatus: 204,
 };
 
 async function bootstrap() {
