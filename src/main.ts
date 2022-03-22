74434926aa4f5a311c803fc9aba181ae2601404d
rachkovartem <ateee@yandex.ru>
2022-03-22 12:21:46 +0300

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,25 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 const cors = require('cors')
 
-// const corsOptions = {
-//   // origin: ['http://localhost:3000', 'https://nextchat-app.herokuapp.com', '193.176.84.208'],
-//   credentials: true,
-//   // optionSuccessStatus: 200,
-//   origin: "*",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-// };
+const corsOptions = {
+  // origin: ['http://localhost:3000', 'https://nextchat-app.herokuapp.com', '193.176.84.208'],
+  credentials: true,
+  // optionSuccessStatus: 200,
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors())
-  // app.enableCors(corsOptions);
+  app.enableCors(corsOptions);
   app.use(cookieParser());
+  app.use( (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+    res.header('Access-Control-Allow-Origin', 'Content-Type, Accept');
+  } );
   await app.listen(process.env.PORT || 8080, () => console.log(process.env.PORT));
 }
 bootstrap();
