2b3ad54c9997ca77da860aef008f713fca0f4593
rachkovartem <ateee@yandex.ru>
2022-04-04 18:19:47 +0300

    fix: delete useless import

--- src/roomsModule/rooms.module.ts
+++ src/roomsModule/rooms.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import {Room} from "./rooms.entity";
+import { Room } from "./rooms.entity";
 import { roomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {User} from "../usersModule/user.entity";
-import {UsersService} from "../usersModule/users.service";
 import {UsersModule} from "../usersModule/users.module";
 import {MessagesModule} from "../messagesModule/messages.module";
 
